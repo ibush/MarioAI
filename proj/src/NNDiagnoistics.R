d134e5c85499240567f1845522c26ed375c1985a
matt <matthew.chen2691@gmail.com>
2015-11-28 22:53:11 -0800

    error graph added - NN has a bad approx of value function

--- proj/src/NNDiagnoistics.R
+++ proj/src/NNDiagnoistics.R
@@ -11,6 +11,8 @@ proj_dir <- paste0(wd,'proj/')
 data_dir <- paste0(wd, 'bin/stats/')
 out_dir  <- paste0(proj_dir, 'writeup/imgs/')
 
+epoch_size <- 1000
+
 # Plot weight stats of fully connected layer
 plot_fc <- function(weight_stats, name){
   fc <- subset(weight_stats, lname==name)
@@ -28,11 +30,25 @@ weight_stats <- read.csv(paste0(data_dir, 'NNweights_NNAgent'))
 weight_stats$X <- NULL
 names(weight_stats) <- c('lname','vmean','variance','vmin','vmax')
 
+pdf(paste0(out_dir, 'fc1_w.pdf'))
+plot_fc(weight_stats, 'fc1')
+dev.off()
+
+
+pdf(paste0(out_dir, 'fc2_w.pdf'))
 plot_fc(weight_stats, 'fc2')
+dev.off()
+
 
 # Plot error
 names(metric_stats) <- c('stat','value')
 error_stats <- subset(metric_stats, stat=='error')
 error_stats$iter <- 1:nrow(error_stats)
-g <- ggplot(error_stats, aes(x=iter,y=value)) + geom_line()
+error_stats$value <- abs(error_stats$value)
+error_stats$epoch <- floor(error_stats$iter / epoch_size)
+error_stats <- summarise(group_by(error_stats, epoch), value=mean(value))
+g <- ggplot(error_stats, aes(x=epoch,y=value)) + geom_bar(stat='identity') +
+    labs(x='Epoch',y='Error') + ggtitle('Neural Network Error')
+pdf(paste0(out_dir, 'NNError.pdf'))
 plot(g)
+dev.off()
